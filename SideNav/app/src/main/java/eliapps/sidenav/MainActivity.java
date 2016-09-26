68e313f8f9f31fe687daabe5e6479559bef807d3
Eli Andrew <eliandrew@gwu.edu>
2016-09-26 17:23:30 -0400

    Added more comments to MainActivity

--- SideNav/app/src/main/java/eliapps/sidenav/MainActivity.java
+++ SideNav/app/src/main/java/eliapps/sidenav/MainActivity.java
@@ -13,6 +13,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+// Declare new class that subclasses AppCompactActivity and implements
+// the NavigationView OnNavigationItemSelectedListener protocol
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -20,38 +22,56 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Finds the toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
+        // Set the toolbar found previously as the tool bar for this activity
+        setSupportActionBar(toolbar);
+        // Find the floating action button
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        // Set the on click listener for the floating action button
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // Creates new view that appears on the bottom of the screen when the
+                // floating action button is pressed
                 Snackbar.make(view, "Floating Action Button Action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
 
+        // Find the drawer layout used for the side nav
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        // Set the toggle states for the drawer
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
+        // Find the side navigation view
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+
+        // Set this activity as the listener for the side nav
         navigationView.setNavigationItemSelectedListener(this);
     }
 
+    // Function to run whenever the back button is pressed
+    // This could be an actual back button or just the user tapping
+    // out of the nav bar
     @Override
     public void onBackPressed() {
+        // Find the drawer layout
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        // See if the drawer was open
         if (drawer.isDrawerOpen(GravityCompat.START)) {
+            // Close the drawer if it was open
             drawer.closeDrawer(GravityCompat.START);
         } else {
             super.onBackPressed();
         }
     }
 
+    // Function to create the menu options
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -59,6 +79,7 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
+    // Function for dealing with options items that were selected
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -74,12 +95,16 @@ public class MainActivity extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 
+    // Function for dealing with navigation items that were selected
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
+        // Below are all the different navigation selection items
+        // Any code that should run when a row is pressed should be
+        // added in the appropriate if statement
         if (id == R.id.nav_camera) {
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
@@ -94,6 +119,7 @@ public class MainActivity extends AppCompatActivity
 
         }
 
+        // Find the drawer layout and close it
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
